2b7b54811773875daf9c58999b3b00b06999e92f
Meemom <jemima.silaen@gmail.com>
2025-09-21 15:49:26 -0400

    feed.py

--- app/routes/feed.py
+++ app/routes/feed.py
@@ -1,1 +1,9 @@
 
+from fastapi import APIRouter, Depends, Request
+from fastapi.responses import JSONResponse
+
+router = APIRouter()
+
+@router.get(f"/feed/action")
+def feed_action(request: Request):
+    return JSONResponse({"message": "Feed action successful"})
